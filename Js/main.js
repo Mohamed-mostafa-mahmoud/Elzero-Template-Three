1e5ce3fe869bda49f2561f5df123b457fe43afba
Mohamed-mostafa-mahmoud <mohamedmgad73@gmail.com>
2021-10-29 11:20:45 -0700

    Upload Design

---
+++ Js/main.js
@@ -0,0 +1,11 @@
+// wow js 
+new WOW().init();
+
+// nicescroll 
+$("body").niceScroll({
+    cursorcolor:"#2395f5",
+    cursorwidth:"10px" ,
+    cursoropacitymin:"1" ,
+    cursorminheight: 90 ,
+    scrollspeed: 30
+  });
